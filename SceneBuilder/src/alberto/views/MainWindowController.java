69aa86fd722b30a9ec76e046230a43da35884487
lasgateras26 <lasgateras2@gmail.com>
2019-10-17 19:09:38 +0200

    añadir partidos OK

--- SceneBuilder/src/alberto/views/MainWindowController.java
+++ SceneBuilder/src/alberto/views/MainWindowController.java
@@ -12,28 +12,34 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.TableView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainWindowController implements Initializable {
+
     @FXML
     private Menu menu_alta;
     @FXML
     private TableView<Partido> tv_partidos;
 
     @FXML
-    void altaPartido(ActionEvent event) {
+    void añadirPartido(ActionEvent event) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("PantallaAlta.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("PantallaAlta.fxml"));
+            Parent root = fxmlLoader.load();
+            PantallaAltaController controller = fxmlLoader.getController();
+            Partido partido = tv_partidos.getSelectionModel().getSelectedItem();
+            controller.setPartidoModificar(partido);
             Stage stage = new Stage();
             stage.initModality(Modality.WINDOW_MODAL);
             stage.setScene(new Scene(root, 700, 400));
             stage.setTitle("Alta Partido");
             stage.show();
         }
-        catch(IOException e){
-                e.printStackTrace();
+        catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
--- SceneBuilder/src/alberto/views/PantallaAltaController.java
+++ SceneBuilder/src/alberto/views/PantallaAltaController.java
@@ -1,12 +1,25 @@
 package alberto.views;
 
+import alberto.logic.Logica;
+import alberto.logic.Utils;
+import alberto.models.Division;
+import alberto.models.Partido;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 public class PantallaAltaController {
+
+    private Partido partidoModificar;
+    private int indice;
 
     @FXML
     private TextField textFieldLocal;
@@ -29,5 +42,24 @@ public class PantallaAltaController {
     @FXML
     private Button btnAñadir;
 
+    @FXML
+    void altaPartido(ActionEvent event) {
+
+        LocalDate localDate = datePickerFecha.getValue();
+        Date fecha = Utils.convertirToDate(localDate);
+
+        Partido partido = new Partido(textFieldLocal.getText(), textFieldVisitante.getText(), Integer.parseInt(textFieldPuntuacionL.getText()),
+                Integer.parseInt(textFieldPuntuacionV.getText()), (Division) comboBoxDivision.getSelectionModel().getSelectedItem(), fecha);
+        Logica.getInstance().addPartido(partido);
+
+        // Como obtener un stage desde un evento
+        Stage stage = ((Stage)((Node)event.getSource()).getScene().getWindow());
+        stage.close();
+    }
+
+    // todo terminar
+    public void setPartidoModificar(Partido partido){
+        textFieldLocal.setText(partido.getEquipoLocal());
+    }
 }
 
