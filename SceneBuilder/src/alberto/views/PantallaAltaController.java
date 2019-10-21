b789e20b3ec924640a2ef93b3abce17f5df3581f
lasgateras26 <lasgateras2@gmail.com>
2019-10-21 15:35:32 +0200

    comboBox correcto

--- SceneBuilder/src/alberto/views/PantallaAltaController.java
+++ SceneBuilder/src/alberto/views/PantallaAltaController.java
@@ -6,16 +6,19 @@ import alberto.models.Division;
 import alberto.models.Partido;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.net.URL;
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.ResourceBundle;
 
-public class PantallaAltaController {
+public class PantallaAltaController implements Initializable {
 
     private Partido partidoModificar;
     private int indiceModificar;
@@ -33,7 +36,7 @@ public class PantallaAltaController {
     private TextField textFieldVisitante;
 
     @FXML
-    private ComboBox<?> comboBoxDivision;
+    private ComboBox<Division> comboBoxDivision;
 
     @FXML
     private DatePicker datePickerFecha;
@@ -67,16 +70,23 @@ public class PantallaAltaController {
     }
 
 
-    public void setPartidoModificar(Partido partido, int indice){
+    public void setPartidoModificar(Partido partido, int indice) {
         this.partidoModificar = partido;
         this.indiceModificar = indice;
         textFieldLocal.setText(partidoModificar.getEquipoLocal());
         textFieldVisitante.setText(partidoModificar.getEquipoVisitante());
         textFieldPuntuacionL.setText(String.valueOf(partidoModificar.getPuntuacionLocal()));
         textFieldPuntuacionV.setText(String.valueOf(partidoModificar.getPuntuacionVisitante()));
-        //comboBoxDivision.getSelectionModel().select(partidoModificar.getDivision());
+        comboBoxDivision.getSelectionModel().select(partidoModificar.getDivision());
         LocalDate localDate = Utils.convertirToLocalDate(partido.getFecha());
         datePickerFecha.setValue(localDate);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        comboBoxDivision.getItems().add(Division.Primera);
+        comboBoxDivision.getItems().add(Division.Segunda);
+        comboBoxDivision.getItems().add(Division.Tercera);
     }
 }
 
